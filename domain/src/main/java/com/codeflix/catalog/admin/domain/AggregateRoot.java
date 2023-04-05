60a9f1147656beffaf184985443c28185742aa80
eduardo3g <eduardosbrasil10@gmail.com>
2023-04-04 21:52:41 -0300

    feat(domain): implement DDD classes (Entity, ValueObject, AggregaterRoot, Identifier

---
+++ domain/src/main/java/com/codeflix/catalog/admin/domain/AggregateRoot.java
@@ -0,0 +1,7 @@
+package com.codeflix.catalog.admin.domain;
+
+public class AggregateRoot<ID extends Identifier> extends Entity<ID> {
+    public AggregateRoot(final ID id) {
+        super(id);
+    }
+}
---
+++ domain/src/main/java/com/codeflix/catalog/admin/domain/ValueObject.java
@@ -0,0 +1,4 @@
+package com.codeflix.catalog.admin.domain;
+
+public abstract class ValueObject {
+}
